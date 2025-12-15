35e3e2fc36e13ab9329de6741ea4186d32165edf
hevanne <cnaepelnickxevan@gmail.com>
2025-12-15 09:38:33 +0100

    frame de base

--- src/ihm/FramePrincipale.java
+++ src/ihm/FramePrincipale.java
@@ -1,11 +1,19 @@
 package ihm;
 
 import controleur.Controleur;
+import javax.swing.*;
 
-public class FramePrincipale
+public class FramePrincipale extends JFrame
 {
+	private PanelPrincipal panelPrincipal;
+
 	public FramePrincipale(Controleur ctrl)
 	{
-		
+		this.panelPrincipal = new PanelPrincipal();
+		this.setTitle("Application Multimedia");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(800, 600);
+		this.add(panelPrincipal);
+		this.setVisible(true);
 	}
 }
---
+++ src/ihm/PanelPrincipal.java
@@ -0,0 +1,11 @@
+package ihm;
+
+import javax.swing.*;
+
+public class PanelPrincipal extends JPanel
+{
+    public PanelPrincipal()
+    {
+        
+    }
+}
