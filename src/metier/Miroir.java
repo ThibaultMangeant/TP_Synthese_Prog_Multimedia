633acc3fcf1c83b5d1f99adf44bc85cc80b40d54
hevanne <cnaepelnickxevan@gmail.com>
2025-12-15 15:40:38 +0100

    classe miroir avec flip vertical et horizontal

--- src/metier/Miroir.java
+++ src/metier/Miroir.java
@@ -7,5 +7,64 @@ import java.awt.image.BufferedImage;
  */
 public class Miroir
 {
-	
+	String fichierSource;
+	String fichierDest;
+	ImageUtil imgUtil;
+
+	public Miroir(String fichierSource, String fichierDest)
+	{
+		this.fichierSource = fichierSource;
+		this.fichierDest   = fichierDest;
+		this.imgUtil       = new ImageUtil(fichierSource);
+	}
+
+	/**
+	 * Retourne l'image comme dans un miroir horizontal (gauche <-> droite)
+	 */
+	public void miroirHorizontal()
+	{
+		BufferedImage src = this.imgUtil.getImage();
+		int w = src.getWidth();
+		int h = src.getHeight();
+
+		BufferedImage out = new BufferedImage(w, h,
+				src.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : src.getType());
+
+		for (int y = 0; y < h; y++)
+		{
+			for (int x = 0; x < w; x++)
+			{
+				int rgb = src.getRGB(x, y);
+				out.setRGB(w - 1 - x, y, rgb);
+			}
+		}
+
+		this.imgUtil.setImage(out);
+		this.imgUtil.sauvegarderImage(this.fichierDest);
+	}
+
+	/**
+	 * Retourne l'image comme dans un miroir vertical (haut <-> bas)
+	 */
+	public void miroirVertical()
+	{
+		BufferedImage src = this.imgUtil.getImage();
+		int w = src.getWidth();
+		int h = src.getHeight();
+
+		BufferedImage out = new BufferedImage(w, h,
+				src.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : src.getType());
+
+		for (int y = 0; y < h; y++)
+		{
+			for (int x = 0; x < w; x++)
+			{
+				int rgb = src.getRGB(x, y);
+				out.setRGB(x, h - 1 - y, rgb);
+			}
+		}
+
+		this.imgUtil.setImage(out);
+		this.imgUtil.sauvegarderImage(this.fichierDest);
+	}
 }
