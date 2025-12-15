53481490243fe222d5c6a348971bfc1b1e3be596
Thibault Mangeant <166180018+ThibaultMangeant@users.noreply.github.com>
2025-12-15 10:44:20 +0100

    indent tab

--- src/ihm/FramePrincipale.java
+++ src/ihm/FramePrincipale.java
@@ -7,58 +7,58 @@ import java.awt.event.ActionListener;
 
 public class FramePrincipale extends JFrame
 {
-    private PanelPrincipal panelPrincipal;
-    private Controleur controleur;
+	private PanelPrincipal panelPrincipal;
+	private Controleur controleur;
 
-    public FramePrincipale(Controleur ctrl)
-    {
-        this.controleur = ctrl;
-        
-        this.panelPrincipal = new PanelPrincipal();
-        
-        this.setTitle("Application Multimedia");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(800, 600);
-        this.setLocation(200, 200);
-        
-        this.initMenu(); 
+	public FramePrincipale(Controleur ctrl)
+	{
+		this.controleur = ctrl;
+		
+		this.panelPrincipal = new PanelPrincipal();
+		
+		this.setTitle("Application Multimedia");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(800, 600);
+		this.setLocation(200, 200);
+		
+		this.initMenu(); 
 
-        this.add(panelPrincipal);
-        this.setVisible(true);
-    }
+		this.add(panelPrincipal);
+		this.setVisible(true);
+	}
 
-    private void initMenu()
-    {
-        JMenuBar menuBar = new JMenuBar();
+	private void initMenu()
+	{
+		JMenuBar menuBar = new JMenuBar();
 
-        JMenu menuFichier = new JMenu("Fichier");
-        
-        JMenuItem itemOuvrir = new JMenuItem("Ouvrir");
-        JMenuItem itemQuitter = new JMenuItem("Quitter");
+		JMenu menuFichier = new JMenu("Fichier");
+		
+		JMenuItem itemOuvrir = new JMenuItem("Ouvrir");
+		JMenuItem itemQuitter = new JMenuItem("Quitter");
 
-        itemQuitter.addActionListener(new ActionListener()
+		itemQuitter.addActionListener(new ActionListener()
 		{
-            @Override
-            public void actionPerformed(ActionEvent e) 
+			@Override
+			public void actionPerformed(ActionEvent e) 
 			{
-                System.exit(0);
-            }
-        });
-    
-        itemOuvrir.addActionListener(e -> {
-            System.out.println("Clic sur Ouvrir -> Appel au contrôleur");
-        });
+				System.exit(0);
+			}
+		});
+	
+		itemOuvrir.addActionListener(e -> {
+			System.out.println("Clic sur Ouvrir -> Appel au contrôleur");
+		});
 
-        menuFichier.add(itemOuvrir);
-        menuFichier.addSeparator();
-        menuFichier.add(itemQuitter);
+		menuFichier.add(itemOuvrir);
+		menuFichier.addSeparator();
+		menuFichier.add(itemQuitter);
 
-        JMenu menuEdition = new JMenu("Édition");
-        menuEdition.add(new JMenuItem("Paramètres"));
+		JMenu menuEdition = new JMenu("Édition");
+		menuEdition.add(new JMenuItem("Paramètres"));
 
-        menuBar.add(menuFichier);
-        menuBar.add(menuEdition);
+		menuBar.add(menuFichier);
+		menuBar.add(menuEdition);
 
-        this.setJMenuBar(menuBar);
-    }
+		this.setJMenuBar(menuBar);
+	}
 }
--- src/ihm/PanelPrincipal.java
+++ src/ihm/PanelPrincipal.java
@@ -4,8 +4,8 @@ import javax.swing.*;
 
 public class PanelPrincipal extends JPanel
 {
-    public PanelPrincipal()
-    {
-        this.add(new JLabel("Contenu principal de l'application"));
-    }
+	public PanelPrincipal()
+	{
+		this.add(new JLabel("Contenu principal de l'application"));
+	}
 }
