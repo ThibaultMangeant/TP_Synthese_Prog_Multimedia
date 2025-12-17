913490ec6434987449e8673259014b53d51da638
Thibault Mangeant <166180018+ThibaultMangeant@users.noreply.github.com>
2025-12-17 14:41:44 +0100

    barreOutil complete

--- src/ihm/BarreOutil.java
+++ src/ihm/BarreOutil.java
@@ -9,7 +9,6 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import java.awt.event.ActionListener;
-import java.util.concurrent.Flow;
 import java.awt.event.ActionEvent;
 
 /** Barre d'outils de l'application
@@ -17,16 +16,22 @@ import java.awt.event.ActionEvent;
  */
 public class BarreOutil extends JPanel implements ActionListener
 {
-	private JButton btnOuvrir , btnEnregistrer;
-	private JButton btnAnnuler, btnRepeter;
-	private JButton btnZoom   , btnDezoom;
+	private JButton btnOuvrir        , btnEnregistrer;
+	private JButton btnAnnuler       , btnRepeter;
+	private JButton btnZoom          , btnDezoom;
+	private JButton btnRedimensionner, btnRotation;
+	private JButton btnMiroirH       , btnMiroirV;
+	private JButton btnPotPeinture;
 
 	public BarreOutil(FramePrincipale frame)
 	{
 		Dimension dimension;
-		ImageIcon iconOuvrir, iconEnregistrer;
-		ImageIcon iconAnnuler, iconRepeter;
-		ImageIcon iconZoom, iconDezoom;
+		ImageIcon iconOuvrir        , iconEnregistrer;
+		ImageIcon iconAnnuler       , iconRepeter;
+		ImageIcon iconZoom          , iconDezoom;
+		ImageIcon iconRedimensionner, iconRotation;
+		ImageIcon iconMiroirH       , iconMiroirV;
+		ImageIcon iconPotPeinture;
 
 
 		this.setLayout(new FlowLayout(FlowLayout.LEFT));
@@ -35,45 +40,70 @@ public class BarreOutil extends JPanel implements ActionListener
 
 		/* Création des composants */
 		// Icones
-		iconOuvrir      = new ImageIcon("../src/images/icons/iconOuvrir.png");
-		iconEnregistrer = new ImageIcon("../src/images/icons/iconEnregistrer.png");
-		iconAnnuler     = new ImageIcon("../src/images/icons/iconAnnuler.png");
-		iconRepeter     = new ImageIcon("../src/images/icons/iconRepeter.png");
-		iconZoom        = new ImageIcon("../src/images/icons/iconZoom.png");
-		iconDezoom      = new ImageIcon("../src/images/icons/iconDezoom.png");
+		iconOuvrir         = new ImageIcon("../src/images/icons/iconOuvrir.svg"        );
+		iconEnregistrer    = new ImageIcon("../src/images/icons/iconEnregistrer.svg"   );
+		iconAnnuler        = new ImageIcon("../src/images/icons/iconAnnuler.svg"       );
+		iconRepeter        = new ImageIcon("../src/images/icons/iconRepeter.svg"       );
+		iconZoom           = new ImageIcon("../src/images/icons/iconZoom.svg"          );
+		iconDezoom         = new ImageIcon("../src/images/icons/iconDezoom.svg"        );
+		iconRedimensionner = new ImageIcon("../src/images/icons/iconRedimensionner.svg");
+		iconRotation       = new ImageIcon("../src/images/icons/iconRotation.svg"      );
+		iconMiroirH        = new ImageIcon("../src/images/icons/iconMiroirH.svg"       );
+		iconMiroirV        = new ImageIcon("../src/images/icons/iconMiroirV.svg"       );
+		iconPotPeinture    = new ImageIcon("../src/images/icons/iconPotPeinture.svg"   );
 
 		// Boutons
-		this.btnOuvrir      = new JButton(iconOuvrir);
-		this.btnEnregistrer = new JButton(iconEnregistrer);
-		this.btnAnnuler     = new JButton(iconAnnuler);
-		this.btnRepeter	    = new JButton(iconRepeter);
-		this.btnZoom        = new JButton(iconZoom);
-		this.btnDezoom      = new JButton(iconDezoom);
+		this.btnOuvrir         = new JButton(iconOuvrir        );
+		this.btnEnregistrer    = new JButton(iconEnregistrer   );
+		this.btnAnnuler        = new JButton(iconAnnuler       );
+		this.btnRepeter	       = new JButton(iconRepeter       );
+		this.btnZoom           = new JButton(iconZoom          );
+		this.btnDezoom         = new JButton(iconDezoom        );
+		this.btnRedimensionner = new JButton(iconRedimensionner);
+		this.btnRotation       = new JButton(iconRotation      );
+		this.btnMiroirH        = new JButton(iconMiroirH       );
+		this.btnMiroirV        = new JButton(iconMiroirV       );
+		this.btnPotPeinture    = new JButton(iconPotPeinture   );
 
 		// Configuration des boutons
 		dimension = new Dimension(40, 40);
-		this.btnOuvrir     .setPreferredSize(dimension);
-		this.btnEnregistrer.setPreferredSize(dimension);
-		this.btnAnnuler    .setPreferredSize(dimension);
-		this.btnRepeter    .setPreferredSize(dimension);
-		this.btnZoom       .setPreferredSize(dimension);
-		this.btnDezoom     .setPreferredSize(dimension);
+		this.btnOuvrir        .setPreferredSize(dimension);
+		this.btnEnregistrer   .setPreferredSize(dimension);
+		this.btnAnnuler       .setPreferredSize(dimension);
+		this.btnRepeter       .setPreferredSize(dimension);
+		this.btnZoom          .setPreferredSize(dimension);
+		this.btnDezoom        .setPreferredSize(dimension);
+		this.btnRedimensionner.setPreferredSize(dimension);
+		this.btnRotation      .setPreferredSize(dimension);
+		this.btnMiroirH       .setPreferredSize(dimension);
+		this.btnMiroirV       .setPreferredSize(dimension);
+		this.btnPotPeinture   .setPreferredSize(dimension);
 
 		/* Ajout des composants */
-		this.add(this.btnOuvrir);
-		this.add(this.btnEnregistrer);
-		this.add(this.btnAnnuler);
-		this.add(this.btnRepeter);
-		this.add(this.btnZoom);
-		this.add(this.btnDezoom);
+		this.add(this.btnOuvrir        );
+		this.add(this.btnEnregistrer   );
+		this.add(this.btnAnnuler       );
+		this.add(this.btnRepeter       );
+		this.add(this.btnZoom          );
+		this.add(this.btnDezoom        );
+		this.add(this.btnRedimensionner);
+		this.add(this.btnRotation      );
+		this.add(this.btnMiroirH       );
+		this.add(this.btnMiroirV       );
+		this.add(this.btnPotPeinture   );
 
 		/* Activation des composants */
-		this.btnOuvrir     .addActionListener(this);
-		this.btnEnregistrer.addActionListener(this);
-		this.btnAnnuler    .addActionListener(this);
-		this.btnRepeter    .addActionListener(this);
-		this.btnZoom       .addActionListener(this);
-		this.btnDezoom     .addActionListener(this);
+		this.btnOuvrir        .addActionListener(this);
+		this.btnEnregistrer   .addActionListener(this);
+		this.btnAnnuler       .addActionListener(this);
+		this.btnRepeter       .addActionListener(this);
+		this.btnZoom          .addActionListener(this);
+		this.btnDezoom        .addActionListener(this);
+		this.btnRedimensionner.addActionListener(this);
+		this.btnRotation      .addActionListener(this);
+		this.btnMiroirH       .addActionListener(this);
+		this.btnMiroirV       .addActionListener(this);
+		this.btnPotPeinture   .addActionListener(this);
 	}
 
 	public void actionPerformed(ActionEvent e)
@@ -101,6 +131,26 @@ public class BarreOutil extends JPanel implements ActionListener
 		else if (e.getSource() == this.btnRepeter)
 		{
 			System.out.println("Répéter !");
+		}
+		else if (e.getSource() == this.btnRedimensionner)
+		{
+			System.out.println("Redimensionner !");
+		}
+		else if (e.getSource() == this.btnRotation)
+		{
+			System.out.println("Rotation !");
+		}
+		else if (e.getSource() == this.btnMiroirH)
+		{
+			System.out.println("Miroir Horizontal !");
+		}
+		else if (e.getSource() == this.btnMiroirV)
+		{
+			System.out.println("Miroir Vertical !");
+		}
+		else if (e.getSource() == this.btnPotPeinture)
+		{
+			System.out.println("Pot de Peinture !");
 		}
 	}
 }
